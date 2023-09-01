ff86bf0bbe7aed3f35f0768fe2900cb37c3dcea9
LitaAmelia <litaamelia126@gmail.com>
2023-09-01 14:11:58 +0700

    first commit

--- src/test/java/stepDefinition/RunTest.java
+++ src/test/java/stepDefinition/RunTest.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "stepDefinition",
-        tags = "@Sorting",
-        plugin = {"pretty", "html:target/SortingReport.html"}
+        tags = "@Login",
+        plugin = {"pretty", "html:target/LoginReport.html"}
 )
 public class RunTest {
 
